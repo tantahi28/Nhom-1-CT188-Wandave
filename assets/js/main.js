d8b5cdbf8621b40b1261b835e82a42243accabe6
Tantahi <nguyentan280202@gmail.com>
2022-05-09 15:19:45 +0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,20 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
-
-// Search
-var searchBlock = $('.header__search-dropdown');
-
-function displaySearch() {
-  searchBlock.classList.add('show');
+//######################## Navigation mobile########################
+var menuAction = $('.menu-action');
+var navMobile = $('.header__nav--mobile');
+menuAction.onclick = function() {
+  navMobile.classList.toggle('show');
 }
+
+
+// ####################### Search #############################
+var searchBlock = $('.header__search-dropdown');
+var actionSearch = $('.action-search');
+actionSearch.onclick = function() {
+  searchBlock.classList.toggle('show');
+}
+
 //############################ Back to top ################################
 
 var backtoTop = $('#wd-up');
@@ -19,9 +27,10 @@ window.addEventListener('scroll' , function(){
   }
 })
 
-function toTop() {  
-  window.scrollTo(0, 0);  
-}
+backtoTop.onclick = function() {
+  window.scrollTo(0, 0); 
+};
 
+//######################### 
 // ######################## Slider #############################
 
